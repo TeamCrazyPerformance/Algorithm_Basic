87c59ec0b1b357beafeb1deb3e02421de1cce735
rhrudska987 <59828706+rhrudska987@users.noreply.github.com>
2023-03-12 16:46:55 +0900

    Baekjoon_2167 rhrudska987 - Refactor 1st Solution

--- 고경남/1주차/2167.java
+++ 고경남/1주차/2167.java
@@ -1,27 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class 이차원_배열의_합 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
 		
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
 		int NM_arr[][] = new int[N][M];
-		
 		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				NM_arr[i][j] = sc.nextInt();
-			}
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) 
+				NM_arr[i][j] = Integer.parseInt(st.nextToken());
 		}
 		
-		int K = sc.nextInt();
+		int K = Integer.parseInt(br.readLine());
 		int K_arr[][] = new int[K][4];
 		
 		for(int i = 0; i < K; i++) {
-			for(int j = 0; j < 4; j++) {
-				K_arr[i][j] = sc.nextInt();
-			}
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 4; j++) 
+				K_arr[i][j] = Integer.parseInt(st.nextToken());
 		}
 		
 		for(int l = 0; l < K; l++) {
@@ -37,7 +48,10 @@ public class 이차원_배열의_합 {
 					sum += NM_arr[i-1][j-1];
 				j = tmp;
 			}
-			System.out.println(sum);
+			bw.write(String.valueOf(sum) + "\n");
 		}
-	}
+		bw.flush();
+		bw.close();
+		br.close();
+  }
 }
