943cba2e5db4a6914469cf9d47bbe57a45879a22
rhrudska987 <59828706+rhrudska987@users.noreply.github.com>
2023-03-12 15:26:35 +0900

    Create 2562.java
    Baekjoon_1874 rhrudska987 - Add 1nd Solution

---
+++ 고경남/1주차/2562.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner	sc = new Scanner(System.in);
+		
+		int arr[] = new int[9];
+		for(int i=0; i<9; i++) {
+			arr[i] = sc.nextInt(); 
+		}
+		
+		
+		int idx = 0;
+		int max = 0;
+		
+		for(int i=0; i<9; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+				idx = i;
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(idx + 1);
+
+	}
+}
