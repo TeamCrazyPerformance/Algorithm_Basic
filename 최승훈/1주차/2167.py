a657c5b2dc303a4f002a6aa8e8acf5b0e4d5bb74
cshooon <113033780+cshooon@users.noreply.github.com>
2023-03-14 12:31:55 +0900

    add 2167

--- 최승훈/1주차/2167.py
+++ 최승훈/1주차/2167.py
@@ -3,7 +3,7 @@ maps = [[] for _ in range(n)]
 
 for i in range(n):
     data = list(map(int, input().split()))
-    maps[i].extend(data) # append 그대로 들어감 extend 리스트 안의 원소가 들어감
+    maps[i].extend(data) # append 그대로 들어감 extend 리스트 안의 원소가 들어감 ㅎㅎ
 
 for _ in range(int(input())):
     a, b, c, d = map(int, input().split())
