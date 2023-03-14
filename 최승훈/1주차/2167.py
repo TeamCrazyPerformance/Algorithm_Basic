69ab504e6a96e85c177f327dcf95d60e9659a53c
cshooon <113033780+cshooon@users.noreply.github.com>
2023-03-14 12:30:58 +0900

    add 1st solution

---
+++ 최승훈/1주차/2167.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+maps = [[] for _ in range(n)]
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    maps[i].extend(data) # append 그대로 들어감 extend 리스트 안의 원소가 들어감
+
+for _ in range(int(input())):
+    a, b, c, d = map(int, input().split())
+    total = 0
+    for i in range(a - 1, c):
+        for j in range(b - 1, d):
+            total += maps[i][j]
+
+    print(total)
