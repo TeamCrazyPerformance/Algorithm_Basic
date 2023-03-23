081161128ad7ae4e62235e60b3bb180d7a8d90d2
0HyerinPark <97073081+0HyerinPark@users.noreply.github.com>
2023-03-23 18:55:53 +0900

    Update 1874.py

--- 박혜린/2주차/1874.py
+++ 박혜린/2주차/1874.py
@@ -1,1 +1,22 @@
+n = int(input())
+stack, ans = [], []
+cur = 1
+isPossible = True
 
+for i in range(n):
+  num = int(input())
+  while cur <= num: # push
+    stack.append(cur)
+    ans.append('+')
+    cur += 1
+  if stack[-1] == num: # pop
+    stack.pop()
+    ans.append('-')
+  else:
+    isPossible = False
+
+if isPossible :
+  for i in ans:
+    print(i)
+else:
+  print('NO')
