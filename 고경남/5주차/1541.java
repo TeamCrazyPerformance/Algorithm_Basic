4940814119c6be05f7c1dde13675f6ed57a071cd
rhrudska987 <59828706+rhrudska987@users.noreply.github.com>
2023-04-24 15:28:59 +0900

    add: 1541

---
+++ 고경남/5주차/1541.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class 잃어버린_괄호 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String sub[] = sc.nextLine().split("-");
+		
+		int answer = 0;
+		boolean check_first = true;
+		for(int i=0; i<sub.length; i++) {
+			int sum = 0;
+			
+			String add[] = sub[i].split("\\+");
+			
+			for(int j=0; j<add.length; j++)
+				sum += Integer.parseInt(add[j]);
+			
+			if(check_first) { 
+				answer = sum; //처음에는 무조건 양수값만 올 수 있음
+				check_first = false;
+			}
+			else 
+				answer -= sum;
+		}
+		System.out.println(answer);
+	}
+}
