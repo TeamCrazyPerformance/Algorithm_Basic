69f34a72222df71eedd0fdcc08918d2b81c72cf3
rhrudska987 <59828706+rhrudska987@users.noreply.github.com>
2023-04-26 18:42:09 +0900

    add: 1931

---
+++ 고경남/5주차/1931.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class 회의실_배정 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] meeting = new int[N][2];
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			meeting[i][0] = Integer.parseInt(st.nextToken());
+			meeting[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(meeting, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[1] == o2[1])
+					return o1[0] - o2[0];
+				return o1[1] - o2[1];
+			}
+		});
+		
+		int answer = 0;		
+		int end = 0;
+		
+		for(int i=0; i<N; i++) {
+			if(meeting[i][0] >= end) {
+				answer++;
+				end = meeting[i][1];
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
