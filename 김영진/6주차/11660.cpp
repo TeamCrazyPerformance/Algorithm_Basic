9faee7839d2e060b2689e070b602269e7d8fc695
김영진 <126306228+youngjin0305@users.noreply.github.com>
2023-05-02 15:45:57 +0900

    add: 11660 구간 합 구하기 5

---
+++ 김영진/6주차/11660.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int num[1025][1025];
+int dp[1025][1025]; // 0,0~i,j까지의 합
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> num[i][j];
+			dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + num[i][j];
+		}
+	}
+	for (int i = 0; i < m; i++ ) {
+		int x1, x2, y1, y2;
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << "\n";
+	}
+	return 0;
+}
