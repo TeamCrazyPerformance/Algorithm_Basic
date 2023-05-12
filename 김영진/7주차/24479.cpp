430966aeb6962b8334e08740cecb7a5562a33dba
김영진 <126306228+youngjin0305@users.noreply.github.com>
2023-05-12 16:03:40 +0900

    add: 24479 알고리즘 수업 - 깊이 우선 탐색 1

---
+++ 김영진/7주차/24479.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+vector<pair<int, int>> edge;
+int cnt = 1;
+int visited[100001] = { 0 };
+int location[100001] = { 0 };
+int start;
+
+void DFS(int v) {
+	if (visited[v]) return;
+	else {
+		visited[v] = cnt++;
+		for (int i = location[v]; i < edge.size() && edge[i].first == v; i++) DFS(edge[i].second);
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	int n, m, r;
+	cin >> n >> m >> r;
+	int a, b;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		edge.push_back(make_pair(a, b));
+		edge.push_back(make_pair(b, a));
+	}
+	sort(edge.begin(), edge.end());
+	start = edge[0].first;
+	for (int i = 1; i < edge.size(); i++) {
+		if (edge[i].first != edge[i - 1].first) location[edge[i].first] = i;
+	}
+	DFS(r);
+	for (int i = 1; i <= n; i++) cout << visited[i] << "\n";
+	return 0;
+}
